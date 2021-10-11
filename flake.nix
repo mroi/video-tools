2e372dbc11745f06aa8766b0c4ef338603b8437b
Michael Roitzsch <reactorcontrol@icloud.com>
2021-10-11 19:42:45 +0200

    flake: change to pkgs variable

--- flake.nix
+++ flake.nix
@@ -7,18 +7,18 @@
 		};
 	};
 	outputs = { self, nixpkgs, ffmpeg }: {
-		ffmpeg =
-			with import nixpkgs { system = "x86_64-darwin"; };
-			stdenv.mkDerivation {
-				name = "ffmpeg-4.4";
-				src = ffmpeg;
-				nativeBuildInputs = [ llvmPackages_latest.clang yasm ];
-				buildInputs = [ darwin.apple_sdk.frameworks.OpenCL ];
-				configureFlags = "--cc=clang --cpu=corei7-avx --enable-gpl --enable-version3 --enable-nonfree --enable-opencl --enable-static --disable-doc --disable-programs --enable-ffmpeg";
-				installPhase = ''
-					mkdir -p $out/bin
-					cp ffmpeg $out/bin/
-				'';
-			};
+		packages.x86_64-darwin.ffmpeg = let
+			pkgs = import nixpkgs { system = "x86_64-darwin"; };
+		in pkgs.stdenv.mkDerivation {
+			name = "ffmpeg-4.4";
+			src = ffmpeg;
+			nativeBuildInputs = [ pkgs.llvmPackages_latest.clang pkgs.yasm ];
+			buildInputs = [ pkgs.darwin.apple_sdk.frameworks.OpenCL ];
+			configureFlags = "--cc=clang --cpu=corei7-avx --enable-gpl --enable-version3 --enable-nonfree --enable-opencl --enable-static --disable-doc --disable-programs --enable-ffmpeg";
+			installPhase = ''
+				mkdir -p $out/bin
+				cp ffmpeg $out/bin/
+			'';
+		};
 	};
 }
