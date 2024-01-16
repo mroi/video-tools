0066936b299bfbf1d7409157b06a4ab38b2c769f
Michael Roitzsch <reactorcontrol@icloud.com>
2024-01-17 00:11:11 +0100

    restructure and update flake
    • load build-time dependencies using regular fetchFromGitHub
    • fix FFmpeg build
    • workaround for LLVM compiler error with mp4box

--- flake.nix
+++ flake.nix
@@ -1,21 +1,6 @@
 {
 	description = "video processing tools";
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-		atomicparsley = {
-			url = "github:wez/atomicparsley/20210715.151551.e7ad03a";
-			flake = false;
-		};
-		ffmpeg = {
-			url = "github:FFmpeg/FFmpeg/n6.1";
-			flake = false;
-		};
-		mp4box = {
-			url = "github:gpac/gpac/v2.0.0";
-			flake = false;
-		};
-	};
-	outputs = { self, nixpkgs, atomicparsley, ffmpeg, mp4box }: let
+	outputs = { self, nixpkgs }: let
 		systems = [ "x86_64-linux" "x86_64-darwin" ];
 		lib = import "${nixpkgs}/lib";
 		forAll = list: f: lib.genAttrs list f;
@@ -25,25 +10,39 @@
 
 			default = self.outputs.packages.${system}.video-tools;
 
-			atomicparsley = stdenv.mkDerivation {
-				name = "atomicparsley-${lib.substring 0 8 self.inputs.atomicparsley.lastModifiedDate}";
-				src = atomicparsley;
+			atomicparsley = stdenv.mkDerivation rec {
+				pname = "atomicparsley";
+				version = "20210715";
+				src = fetchFromGitHub {
+					owner = "wez";
+					repo = "atomicparsley";
+					rev = "${version}.151551.e7ad03a";
+					hash = "sha256-77yWwfdEul4uLsUNX1dLwj8K0ilcuBaTVKMyXDvKVx4=";
+				};
 				nativeBuildInputs = [ cmake ];
 				buildInputs = lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Cocoa ];
-				cmakeFlags = [ "-DPACKAGE_VERSION=${lib.substring 0 8 self.inputs.atomicparsley.lastModifiedDate}" ];
+				cmakeFlags = [ "-DPACKAGE_VERSION=${version}" ];
 			};
 
-			ffmpeg = clangStdenv.mkDerivation {
-				name = "ffmpeg";
-				src = ffmpeg;
+			ffmpeg = clangStdenv.mkDerivation rec {
+				pname = "ffmpeg";
+				version = "6.1.1";
+				src = fetchFromGitHub {
+					owner = "FFmpeg";
+					repo = "FFmpeg";
+					rev = "n${version}";
+					hash = "sha256-Q0c95hbCVUHQWPoh5uC8uzMylmB4BnWg+VhXEgSouzo=";
+				};
 				nativeBuildInputs = [ yasm ];
 				buildInputs = lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.OpenCL ];
 				configureFlags = [
-					"--cc=clang --cpu=corei7-avx"
+					"--cc=clang --cxx=clang++ --cpu=corei7-avx"
 					"--enable-gpl --enable-version3 --enable-nonfree"
-					"--enable-opencl --enable-static --disable-doc"
-					"--disable-programs --enable-ffmpeg"
+					"--disable-doc --disable-programs --enable-ffmpeg"
+					"--enable-static --disable-stripping"
+					"--enable-opencl"
 				];
+				enableParallelBuilding = true;
 				installPhase = ''
 					mkdir -p $out/bin
 					cp ffmpeg $out/bin/
@@ -73,17 +72,29 @@
 				'';
 			};
 
-			mp4box = stdenv.mkDerivation {
-				name = "mp4box-2.0.0";
-				src = mp4box;
+			mp4box =
+			# TODO: LLVM compilation error fixed in version 18
+			# https://github.com/llvm/llvm-project/issues/62254
+			assert lib.versionOlder clang.version "18";
+			llvmPackages_14.stdenv.mkDerivation rec {
+				pname = "mp4box";
+				version = "2.0.0";
+				src = fetchFromGitHub {
+					owner = "gpac";
+					repo = "gpac";
+					rev = "v${version}";
+					hash = "sha256-MIX32lSqf/lrz9240h4wMIQp/heUmwvDJz8WN08yf6c=";
+				};
 				nativeBuildInputs = [ pkg-config ] ++ lib.optionals stdenv.isDarwin [ darwin.cctools ];
 				buildInputs = [ zlib ] ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.Carbon ];
+				enableParallelBuilding = true;
 			};
 
-			subler = stdenvNoCC.mkDerivation {
-				name = "subler-1.7.3";
+			subler = stdenvNoCC.mkDerivation rec {
+				pname = "subler";
+				version = "1.7.3";
 				src = fetchurl {
-					url = "https://bitbucket.org/galad87/subler/downloads/Subler-1.7.3.zip";
+					url = "https://bitbucket.org/galad87/subler/downloads/Subler-${version}.zip";
 					sha256 = "sha256-VmxoTUUDYYNrJYyzBKw3QIrLHbtlUuh2cfm9ObJKAS0=";
 				};
 				nativeBuildInputs = [ unzip ];
